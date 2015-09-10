b196423026c9f0975a1571e53b6c14bb350a58a5
ciaorachel <ciaorachel@gmail.com>
2015-09-10 15:00:00 -0500

    updated CalendarEvent model

--- app/models/CalendarEvent.php
+++ app/models/CalendarEvent.php
@@ -1,7 +1,28 @@
 <?php
 
-class CalendarEvent extends \Eloquent {
+use \Esensi\Model\Model;
+
+class CalendarEvent extends Model {
 	protected $fillable = [];
 
 	protected $table = 'calendar_events';
+
+	protected $rules = array(
+	
+	);
+
+	protected $fillable = array(
+
+	);
+
+	public function getDates()
+	{
+		return array_merge(parent::getDates(), 'starts_at', 'ends_at');
+	}
+
+	public function creator()
+	{
+		return $this->belongsTo('User', 'user_id');
+	}
+
 }
