89ecc7c88c01b0c9266450a7b6a554bab40bc564
j-beere <beere.justin@gmail.com>
2015-09-14 15:50:27 -0500

    added registration controller for registering new users

--- app/Acme/Forms/RegistrationForm.php
+++ app/Acme/Forms/RegistrationForm.php
@@ -5,7 +5,6 @@ use Laracasts\Validation\FormValidator;
 class RegistrationForm extends FormValidator {
 
 	protected $rules = [
-		'username' => 'required|unique:users',
 		'email'    => 'required|email|unique:users',
 		'password' => 'required|confirmed'
 	];
